ac229a8a746048b2f30d03f0fdac6f207f1acf19
Navin Pai <lifeofnavin@gmail.com>
2022-09-29 04:14:05 +0530

    [MINOR] Associate user and organization

--- models/organization.js
+++ models/organization.js
@@ -9,6 +9,11 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.hasMany(models.user, {
+        foreignKey: {
+          name: "orgId",
+        },
+      });
     }
   }
   organization.init(
