31f8ae84bd2c517929a8482902acd3dc708f2f54
Navin Pai <lifeofnavin@gmail.com>
2022-10-18 02:28:50 +0530

    Adding enums for userStatus, linkTypes and  userRoles

--- utils/defaults.ts
+++ utils/defaults.ts
@@ -4,11 +4,22 @@ const config = require(__dirname + "/../config/config.json")[env];
 export const DEFAULT_ORG_HERO = "go";
 export const MIN_PASSWORD_LENGTH: number = 6;
 
-export const enum userStatus {
+export enum userStatus {
   invited = "invited",
   active = "active",
   deactivated = "deactivated",
   locked = "locked",
+}
+
+export enum userRoles {
+  user = "user",
+  admin = "admin",
+  global_admin = "global_admin",
+}
+
+export enum linkTypes {
+  static = "static",
+  dynamic = "dynamic",
 }
 
 export const getStaticTemplateVars = () => {
