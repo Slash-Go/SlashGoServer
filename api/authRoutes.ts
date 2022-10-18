8c22a2c8238e0e12c4b042e72577570ef6077bb1
Navin Pai <lifeofnavin@gmail.com>
2022-10-18 20:26:49 +0530

    Adding methods for password reset requests

--- api/authRoutes.ts
+++ api/authRoutes.ts
@@ -1,9 +1,16 @@
 import express from "express";
-import { login, refreshToken } from "../controllers/authController";
+import {
+  login,
+  refreshToken,
+  resetPassword,
+  resetPasswordRequest,
+} from "../controllers/authController";
 
 export const router = express.Router();
 
 router.post("/login", login);
 router.post("/refresh", refreshToken);
+router.post("/reset_password_request", resetPasswordRequest);
+router.post("/reset_password", resetPassword);
 
 module.exports = { router };
--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import db, { sequelize } from "../models";
+import { v4 as uuidv4, validate as isValidUUID } from "uuid";
 import { generateTokenPair } from "../middleware/authMiddleware";
 import { Op } from "sequelize";
-import { userStatus } from "../utils/defaults";
+import { MIN_PASSWORD_LENGTH, userStatus } from "../utils/defaults";
+import { sendMail } from "../services/email";
 
 export const login = (req: Request, res: Response) => {
   const DB: any = db;
@@ -127,6 +129,135 @@ export const refreshToken = (req: Request, res: Response) => {
       }
     })
     .catch(() => {
-      return res.status(401).json({ error: "Unknown Error in Token Refresh" });
+      return res.status(500).json({ error: "Unknown Error in Token Refresh" });
+    });
+};
+
+export const resetPasswordRequest = (req: Request, res: Response) => {
+  const DB: any = db;
+  const { otp, user, organization } = DB;
+
+  const email = req.body["email"];
+
+  if (email == null) {
+    return res
+      .status(401)
+      .json({ error: "Required parameter `email` not provided or null" });
+  }
+
+  user
+    .findOne({
+      attributes: ["id", "email", "orgId", "organization.auth"],
+      where: {
+        email: email,
+        status: userStatus.active,
+      },
+      include: [
+        {
+          model: organization,
+          where: {
+            active: true,
+          },
+          required: true,
+        },
+      ],
+    })
+    .then((data: any) => {
+      if (data != null) {
+        //console.log(data.id);
+        let token = uuidv4();
+        otp
+          .create({
+            operation: "reset-password",
+            token: token,
+            userId: data.id,
+            expiry: sequelize.literal("NOW() + INTERVAL '1D'"),
+            used: false,
+            createdAt: sequelize.fn("NOW"),
+          })
+          .then((otp_data: typeof otp) => {
+          })
+          .catch((e: any) => {
+            console.log("Error in creating OTP: ", e);
+            throw Error("Error in creating OTP");
+          });
+      }
+      return res.status(200).json({
+        message: "If user is active, password reset email has been sent!",
+      });
+    })
+    .catch(() => {
+      return res.status(500).json({ error: "Unknown Error in Reset Password" });
+    });
+};
+
+export const resetPassword = (req: Request, res: Response) => {
+  const DB: any = db;
+  const { otp, user } = DB;
+
+  const token = req.body["token"];
+  const userId = req.body["userId"];
+  const password = req.body["password"];
+
+  if (userId == null) {
+    return res
+      .status(401)
+      .json({ error: "Required parameter `userId` not provided or null" });
+  }
+
+  if (token == null) {
+    return res
+      .status(401)
+      .json({ error: "Required parameter `token` not provided or null" });
+  }
+
+  if (password == null || password.length < 6) {
+    return res.status(401).json({
+      error: `Required field \`password\` must have length >= ${MIN_PASSWORD_LENGTH} characters`,
+    });
+  }
+
+  otp
+    .update(
+      { used: true },
+      {
+        where: {
+          userId: userId,
+          token: token,
+          operation: "reset-password",
+          used: false,
+          expiry: {
+            [Op.gte]: sequelize.fn("NOW"),
+          },
+        },
+      }
+    )
+    .then((data: any) => {
+      if (data[0] == 1) {
+        // TODO: Make this async
+        const salt = bcrypt.genSaltSync(10);
+        const hashedPass = bcrypt.hashSync(password, salt);
+        user
+          .update({ password: hashedPass }, { where: { id: userId } })
+          .then(() => {
+            return res.status(200).json({
+              message: "Password reset success!",
+            });
+          })
+          .catch(() => {
+            return res
+              .status(500)
+              .json({ error: "Unknown Error while resetting password" });
+          });
+      } else {
+        return res.status(400).json({
+          error: "Invalid or expired token provided",
+        });
+      }
+    })
+    .catch(() => {
+      return res
+        .status(500)
+        .json({ error: "Unknown Error in Resetting Password" });
     });
 };
