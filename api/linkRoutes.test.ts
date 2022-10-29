72988d66d6273b08a5d7f36c91cec18a0995ec39
Navin Pai <lifeofnavin@gmail.com>
2022-10-30 02:18:01 +0530

    [MINOR] Adding tests for Org1 user data visibility

--- api/linkRoutes.test.ts
+++ api/linkRoutes.test.ts
@@ -70,6 +70,73 @@ describe("GET /link", () => {
     expect(res.body[0].shortLink).toBe("code");
   });
 
+  it("should return 200 OK and 2 links of org1 for Org1 Admin", async () => {
+    const res = await request(app).get("/link").set('Authorization', 'Bearer ORG1_ADMIN_TOKEN');
+    expect(res.statusCode).toBe(200);
+    expect(res.body.length).toBe(2);
+    expect(res.body[0].shortLink).toBe("privateo1admin");
+    expect(res.body[1].shortLink).toBe("publico1admin");
+  });
+
+  it("should return 200 OK and 1 link of org1 for Org1 User1", async () => {
+    const res = await request(app).get("/link").set('Authorization', 'Bearer ORG1_USER1_TOKEN');
+    expect(res.statusCode).toBe(200);
+    expect(res.body.length).toBe(2);
+    expect(res.body[0].shortLink).toBe("privateo1u1");
+    expect(res.body[1].shortLink).toBe("publico1admin");
+  });
+
+  it("should return 200 OK and 1 link of org1 for Org1 User2", async () => {
+    const res = await request(app).get("/link").set('Authorization', 'Bearer ORG1_USER2_TOKEN');
+    expect(res.statusCode).toBe(200);
+    expect(res.body.length).toBe(1);
+    expect(res.body[0].shortLink).toBe("publico1admin");
+  });
+
+  it("should return 200 OK and no links of org2 for Org2 Admin", async () => {
+    const res = await request(app).get("/link").set('Authorization', 'Bearer ORG2_ADMIN_TOKEN');
+    expect(res.statusCode).toBe(200);
+    expect(res.body.length).toBe(0);
+  });
+
+  it("should return 200 OK and no links of org2 for Org2 user1", async () => {
+    const res = await request(app).get("/link").set('Authorization', 'Bearer ORG2_USER1_TOKEN');
+    expect(res.statusCode).toBe(200);
+    expect(res.body.length).toBe(0);
+  });
+});
+
+describe("GET /link", () => {
+  it("should return 200 OK and one link of global org for Global Admin", async () => {
+    const res = await request(app).get("/link").set('Authorization', 'Bearer GLOBAL_ADMIN_TOKEN');
+    expect(res.statusCode).toBe(200);
+    expect(res.body.length).toBe(1);
+    expect(res.body[0].shortLink).toBe("code");
+  });
+
+  it("should return 200 OK and 2 links of org1 for Org1 Admin", async () => {
+    const res = await request(app).get("/link").set('Authorization', 'Bearer ORG1_ADMIN_TOKEN');
+    expect(res.statusCode).toBe(200);
+    expect(res.body.length).toBe(2);
+    expect(res.body[0].shortLink).toBe("privateo1admin");
+    expect(res.body[1].shortLink).toBe("publico1admin");
+  });
+
+  it("should return 200 OK and 1 link of org1 for Org1 User1", async () => {
+    const res = await request(app).get("/link").set('Authorization', 'Bearer ORG1_USER1_TOKEN');
+    expect(res.statusCode).toBe(200);
+    expect(res.body.length).toBe(2);
+    expect(res.body[0].shortLink).toBe("privateo1u1");
+    expect(res.body[1].shortLink).toBe("publico1admin");
+  });
+
+  it("should return 200 OK and 1 link of org1 for Org1 User2", async () => {
+    const res = await request(app).get("/link").set('Authorization', 'Bearer ORG1_USER2_TOKEN');
+    expect(res.statusCode).toBe(200);
+    expect(res.body.length).toBe(1);
+    expect(res.body[0].shortLink).toBe("publico1admin");
+  });
+
 });
 
 export {};
