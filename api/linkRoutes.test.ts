3d6e9f5ea5cd2bb638d115f11531c73f3ed82cfd
Navin Pai <lifeofnavin@gmail.com>
2022-10-30 02:16:31 +0530

    Mock Authenticate method to allow short-circuiting authentication in tests

---
+++ api/linkRoutes.test.ts
@@ -0,0 +1,63 @@
+import { Request, Response, NextFunction } from "express";
+import { Auth } from "../middleware/authMiddleware";
+
+jest.mock('../middleware/authMiddleware', () => {
+  const originalModule = jest.requireActual('../middleware/authMiddleware');
+  return new Proxy(originalModule, {
+    get: (target, property) => {
+      switch (property) {
+        case 'authenticate': {
+          return jest.fn((
+            req: Request,
+            _: Response,
+            next: NextFunction
+          ) => { 
+            if (req.headers.authorization === "Bearer GLOBAL_ADMIN_TOKEN") {
+              req.auth = new Auth({
+                  userRole: "global_admin",
+                  userId: "00000000-0001-3370-0000-000000000001",
+                  orgId: "10000000-0001-3370-0000-000000000000",
+                });
+            } else if (req.headers.authorization === "Bearer ORG1_ADMIN_TOKEN") {
+              req.auth = new Auth({
+                  userRole: "admin",
+                  userId: "00000000-0001-3370-0000-000000000011",
+                  orgId: "10000000-0001-3370-0000-000000000001",
+                });
+            } else if (req.headers.authorization === "Bearer ORG2_ADMIN_TOKEN") {
+              req.auth = new Auth({
+                  userRole: "admin",
+                  userId: "00000000-0001-3370-0000-000000000041",
+                  orgId: "10000000-0001-3370-0000-000000000002",
+                });
+            } else if (req.headers.authorization === "Bearer ORG1_USER1_TOKEN") {
+              req.auth = new Auth({
+                  userRole: "user",
+                  userId: "00000000-0001-3370-0000-000000000021",
+                  orgId: "10000000-0001-3370-0000-000000000001",
+                });
+            } else if (req.headers.authorization === "Bearer ORG1_USER2_TOKEN") {
+              req.auth = new Auth({
+                  userRole: "user",
+                  userId: "00000000-0001-3370-0000-000000000031",
+                  orgId: "10000000-0001-3370-0000-000000000001",
+                });
+            } else if (req.headers.authorization === "Bearer ORG2_USER1_TOKEN") {
+              req.auth = new Auth({
+                  userRole: "user",
+                  userId: "00000000-0001-3370-0000-000000000051",
+                  orgId: "10000000-0001-3370-0000-000000000002",
+                });
+            }
+            next();
+        })
+      }
+        default: {
+          return target[property]
+        }
+      }
+    },
+  })
+});
+
+export {};
