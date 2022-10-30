1facfd401954565dc0932afdc6ec097a02f4a6c3
Navin Pai <lifeofnavin@gmail.com>
2022-10-31 02:49:00 +0530

    [MINOR] Add tests for data visibility for Org2 users

--- api/linkRoutes.test.ts
+++ api/linkRoutes.test.ts
@@ -3,75 +3,85 @@ import { Request, Response, NextFunction } from "express";
 import { app } from "../index";
 import { Auth } from "../middleware/authMiddleware";
 
-jest.mock('../middleware/authMiddleware', () => {
-  const originalModule = jest.requireActual('../middleware/authMiddleware');
+jest.mock("../middleware/authMiddleware", () => {
+  const originalModule = jest.requireActual("../middleware/authMiddleware");
   return new Proxy(originalModule, {
     get: (target, property) => {
       switch (property) {
-        case 'authenticate': {
-          return jest.fn((
-            req: Request,
-            _: Response,
-            next: NextFunction
-          ) => { 
+        case "authenticate": {
+          return jest.fn((req: Request, _: Response, next: NextFunction) => {
             if (req.headers.authorization === "Bearer GLOBAL_ADMIN_TOKEN") {
               req.auth = new Auth({
-                  userRole: "global_admin",
-                  userId: "00000000-0001-3370-0000-000000000001",
-                  orgId: "10000000-0001-3370-0000-000000000000",
-                });
-            } else if (req.headers.authorization === "Bearer ORG1_ADMIN_TOKEN") {
+                userRole: "global_admin",
+                userId: "00000000-0001-3370-0000-000000000001",
+                orgId: "10000000-0001-3370-0000-000000000000",
+              });
+            } else if (
+              req.headers.authorization === "Bearer ORG1_ADMIN_TOKEN"
+            ) {
               req.auth = new Auth({
-                  userRole: "admin",
-                  userId: "00000000-0001-3370-0000-000000000011",
-                  orgId: "10000000-0001-3370-0000-000000000001",
-                });
-            } else if (req.headers.authorization === "Bearer ORG2_ADMIN_TOKEN") {
+                userRole: "admin",
+                userId: "00000000-0001-3370-0000-000000000011",
+                orgId: "10000000-0001-3370-0000-000000000001",
+              });
+            } else if (
+              req.headers.authorization === "Bearer ORG2_ADMIN_TOKEN"
+            ) {
               req.auth = new Auth({
-                  userRole: "admin",
-                  userId: "00000000-0001-3370-0000-000000000041",
-                  orgId: "10000000-0001-3370-0000-000000000002",
-                });
-            } else if (req.headers.authorization === "Bearer ORG1_USER1_TOKEN") {
+                userRole: "admin",
+                userId: "00000000-0001-3370-0000-000000000041",
+                orgId: "10000000-0001-3370-0000-000000000002",
+              });
+            } else if (
+              req.headers.authorization === "Bearer ORG1_USER1_TOKEN"
+            ) {
               req.auth = new Auth({
-                  userRole: "user",
-                  userId: "00000000-0001-3370-0000-000000000021",
-                  orgId: "10000000-0001-3370-0000-000000000001",
-                });
-            } else if (req.headers.authorization === "Bearer ORG1_USER2_TOKEN") {
+                userRole: "user",
+                userId: "00000000-0001-3370-0000-000000000021",
+                orgId: "10000000-0001-3370-0000-000000000001",
+              });
+            } else if (
+              req.headers.authorization === "Bearer ORG1_USER2_TOKEN"
+            ) {
               req.auth = new Auth({
-                  userRole: "user",
-                  userId: "00000000-0001-3370-0000-000000000031",
-                  orgId: "10000000-0001-3370-0000-000000000001",
-                });
-            } else if (req.headers.authorization === "Bearer ORG2_USER1_TOKEN") {
+                userRole: "user",
+                userId: "00000000-0001-3370-0000-000000000031",
+                orgId: "10000000-0001-3370-0000-000000000001",
+              });
+            } else if (
+              req.headers.authorization === "Bearer ORG2_USER1_TOKEN"
+            ) {
               req.auth = new Auth({
-                  userRole: "user",
-                  userId: "00000000-0001-3370-0000-000000000051",
-                  orgId: "10000000-0001-3370-0000-000000000002",
-                });
+                userRole: "user",
+                userId: "00000000-0001-3370-0000-000000000051",
+                orgId: "10000000-0001-3370-0000-000000000002",
+              });
             }
             next();
-        })
-      }
+          });
+        }
         default: {
-          return target[property]
+          return target[property];
         }
       }
     },
-  })
+  });
 });
 
 describe("GET /link", () => {
   it("should return 200 OK and one link of global org for Global Admin", async () => {
-    const res = await request(app).get("/link").set('Authorization', 'Bearer GLOBAL_ADMIN_TOKEN');
+    const res = await request(app)
+      .get("/link")
+      .set("Authorization", "Bearer GLOBAL_ADMIN_TOKEN");
     expect(res.statusCode).toBe(200);
     expect(res.body.length).toBe(1);
     expect(res.body[0].shortLink).toBe("code");
   });
 
   it("should return 200 OK and 2 links of org1 for Org1 Admin", async () => {
-    const res = await request(app).get("/link").set('Authorization', 'Bearer ORG1_ADMIN_TOKEN');
+    const res = await request(app)
+      .get("/link")
+      .set("Authorization", "Bearer ORG1_ADMIN_TOKEN");
     expect(res.statusCode).toBe(200);
     expect(res.body.length).toBe(2);
     expect(res.body[0].shortLink).toBe("privateo1admin");
@@ -79,7 +89,9 @@ describe("GET /link", () => {
   });
 
   it("should return 200 OK and 1 link of org1 for Org1 User1", async () => {
-    const res = await request(app).get("/link").set('Authorization', 'Bearer ORG1_USER1_TOKEN');
+    const res = await request(app)
+      .get("/link")
+      .set("Authorization", "Bearer ORG1_USER1_TOKEN");
     expect(res.statusCode).toBe(200);
     expect(res.body.length).toBe(2);
     expect(res.body[0].shortLink).toBe("privateo1u1");
@@ -87,20 +99,26 @@ describe("GET /link", () => {
   });
 
   it("should return 200 OK and 1 link of org1 for Org1 User2", async () => {
-    const res = await request(app).get("/link").set('Authorization', 'Bearer ORG1_USER2_TOKEN');
+    const res = await request(app)
+      .get("/link")
+      .set("Authorization", "Bearer ORG1_USER2_TOKEN");
     expect(res.statusCode).toBe(200);
     expect(res.body.length).toBe(1);
     expect(res.body[0].shortLink).toBe("publico1admin");
   });
 
   it("should return 200 OK and no links of org2 for Org2 Admin", async () => {
-    const res = await request(app).get("/link").set('Authorization', 'Bearer ORG2_ADMIN_TOKEN');
+    const res = await request(app)
+      .get("/link")
+      .set("Authorization", "Bearer ORG2_ADMIN_TOKEN");
     expect(res.statusCode).toBe(200);
     expect(res.body.length).toBe(0);
   });
 
   it("should return 200 OK and no links of org2 for Org2 user1", async () => {
-    const res = await request(app).get("/link").set('Authorization', 'Bearer ORG2_USER1_TOKEN');
+    const res = await request(app)
+      .get("/link")
+      .set("Authorization", "Bearer ORG2_USER1_TOKEN");
     expect(res.statusCode).toBe(200);
     expect(res.body.length).toBe(0);
   });
@@ -108,14 +126,18 @@ describe("GET /link", () => {
 
 describe("GET /link", () => {
   it("should return 200 OK and one link of global org for Global Admin", async () => {
-    const res = await request(app).get("/link").set('Authorization', 'Bearer GLOBAL_ADMIN_TOKEN');
+    const res = await request(app)
+      .get("/link")
+      .set("Authorization", "Bearer GLOBAL_ADMIN_TOKEN");
     expect(res.statusCode).toBe(200);
     expect(res.body.length).toBe(1);
     expect(res.body[0].shortLink).toBe("code");
   });
 
   it("should return 200 OK and 2 links of org1 for Org1 Admin", async () => {
-    const res = await request(app).get("/link").set('Authorization', 'Bearer ORG1_ADMIN_TOKEN');
+    const res = await request(app)
+      .get("/link")
+      .set("Authorization", "Bearer ORG1_ADMIN_TOKEN");
     expect(res.statusCode).toBe(200);
     expect(res.body.length).toBe(2);
     expect(res.body[0].shortLink).toBe("privateo1admin");
@@ -123,7 +145,9 @@ describe("GET /link", () => {
   });
 
   it("should return 200 OK and 1 link of org1 for Org1 User1", async () => {
-    const res = await request(app).get("/link").set('Authorization', 'Bearer ORG1_USER1_TOKEN');
+    const res = await request(app)
+      .get("/link")
+      .set("Authorization", "Bearer ORG1_USER1_TOKEN");
     expect(res.statusCode).toBe(200);
     expect(res.body.length).toBe(2);
     expect(res.body[0].shortLink).toBe("privateo1u1");
@@ -131,12 +155,29 @@ describe("GET /link", () => {
   });
 
   it("should return 200 OK and 1 link of org1 for Org1 User2", async () => {
-    const res = await request(app).get("/link").set('Authorization', 'Bearer ORG1_USER2_TOKEN');
+    const res = await request(app)
+      .get("/link")
+      .set("Authorization", "Bearer ORG1_USER2_TOKEN");
     expect(res.statusCode).toBe(200);
     expect(res.body.length).toBe(1);
     expect(res.body[0].shortLink).toBe("publico1admin");
   });
 
+  it("should return 200 OK and no links of org2 for Org2 Admin", async () => {
+    const res = await request(app)
+      .get("/link")
+      .set("Authorization", "Bearer ORG2_ADMIN_TOKEN");
+    expect(res.statusCode).toBe(200);
+    expect(res.body.length).toBe(0);
+  });
+
+  it("should return 200 OK and no links of org2 for Org2 user1", async () => {
+    const res = await request(app)
+      .get("/link")
+      .set("Authorization", "Bearer ORG2_USER1_TOKEN");
+    expect(res.statusCode).toBe(200);
+    expect(res.body.length).toBe(0);
+  });
 });
 
 export {};
